f25ee822ac746d278749498479b71f669b16ebe2
Andrii Stafiniak <andriistafiniak@MacBook-Pro-Andrii.local>
2023-04-24 06:13:42 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let button = document.querySelector(".button");
+let mainElement = document.querySelector(".main");
+let themeName = document.querySelector(".button__themeName")
+
+button.addEventListener("click", () => {
+   mainElement.classList.toggle("mainChangeBgCol");
+   themeName.innerText = mainElement.classList.contains("mainChangeBgCol") ? "jasny" : "ciemny";
+});
